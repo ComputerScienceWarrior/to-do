26f502e8c5e6485a32f707df21a0090259b82b52
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-08-31 18:24:11 -0700

    creates user controller with various user actions (show, edit, update, destroy, new, create)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:id] = user.id
+      redirect_to user_path user
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
