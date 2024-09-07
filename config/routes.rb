782da71afcc27c2beedcf4cff5cd3be764d9b411
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-09-07 13:19:04 -0700

    creates route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :lists, only: [:new, :create]
+    resources :lists, only: [:new, :show, :index, :create]
   end
   resources :lists, only: [:index, :show] do
     resources :list_items, only: [:index, :new, :create, :edit, :update, :destroy]
   end
+
+  get '/login', to: 'sessions#create'
 
   root "pages#home"
 end
