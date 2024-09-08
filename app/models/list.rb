1a8c630897954e814963931aadc18fc5e3335e5c
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-09-08 06:24:21 -0700

    updates list model file to accept nested attributes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
-  has_many :list_items
+  has_many :list_items, dependent: :destroy
   belongs_to :user
+  accepts_nested_attributes_for :list_items, allow_destroy: true
 end
