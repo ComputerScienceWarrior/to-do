e598b902660bff7b167fb763eb57c5f5e8f54692
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-09-09 02:26:53 -0700

    updates params for authenticatio of login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in!'
     else
